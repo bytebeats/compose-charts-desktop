a12b3fd5c5f78b3236aa1201ef4d9dad6d7049e6
Chen Pan <happychinapc@gmail.com>
2022-03-10 19:23:42 +0800

    Initial commit

---
+++ src/main/kotlin/me/bytebeats/charts/desktop/Animations.kt
@@ -0,0 +1,15 @@
+package me.bytebeats.charts.desktop
+
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.TweenSpec
+
+/**
+ * @Author bytebeats
+ * @Email <happychinapc@gmail.com>
+ * @Github https://github.com/bytebeats
+ * @Created at 2022/3/10 19:20
+ * @Version 1.0
+ * @Description TO-DO
+ */
+
+fun simpleChartAnimation(): AnimationSpec<Float> = TweenSpec<Float>(durationMillis = 500)
